2443a642ffb74030bdb7a022fdf8f60f29ce08f4
majdzrg <m.zrigui19100@pi.tn>
2021-08-18 01:07:30 +0200

    auth with jwt and passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,22 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TeacherModule } from './teacher/teacher.module';
 import { UserWaitingModule } from './user-waiting/user-waiting.module';
-
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
   imports: [ UserWaitingModule,
              TeacherModule,
-    MongooseModule.forRoot('mongodb+srv://majd:majdzrg123@cluster0.1jkjz.mongodb.net/backend?retryWrites=true&w=majority')],
+    MongooseModule.forRoot('mongodb+srv://majd:majdzrg123@cluster0.1jkjz.mongodb.net/backend?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex:true
+    }),
+    UserModule,
+    AuthModule,
+    
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
